bf680bbb2c1839a38bc66c3d9946de60428ea06a
Parham <parhamallahyar70@gmail.com>
2020-12-12 09:25:00 +0330

    feat(auth/src/Facades/storeCodeFacade)Add storeCodeFacade

---
+++ auth/src/Facades/storeCodeFacade.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Auth\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class storeCodeFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'storeCodeFacade';  
+    }
+}
