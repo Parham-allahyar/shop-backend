587e50131ddbf8b5fcbe29828892758af80a0d33
Parham <parhamallahyar70@gmail.com>
2020-12-12 10:21:14 +0330

    feat(auth/src/Http/Controllers/AuthController)add auth method

--- auth/src/Http/Controllers/AuthController.php
+++ auth/src/Http/Controllers/AuthController.php
@@ -7,7 +7,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth\Facades\userProviderFacade;
 use Auth\Facades\storeCodeFacade;
-
+use Carbon\Carbon;
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 class AuthController extends Controller
 {
     public function login(Request $request)
@@ -40,6 +43,31 @@ class AuthController extends Controller
         //send Code
         NotificationModule::sendsms($user->phone_Number, $code);
         
+    }
+
+
+    public function auth()
+    {
+        $receivedCode = request('code');
+       
+        $userId = storeCodeFacade::getCode($receivedCode);
+        $user = User::find($userId);
+        if ($userId !== null) {
+
+            //Send Response
+            $token = JWTAuth::fromUser($user);
+            return $this->respondWithToken($token);
+        } else {
+            return redirect('/login');
+        }
+    }
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->Carbon::now()->subDays(30)
+        ]);
     }
     
 }
