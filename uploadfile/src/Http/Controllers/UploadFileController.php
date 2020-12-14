d0beaabdb0378f44767e4b4253e17d778b2298ba
Parham <parhamallahyar70@gmail.com>
2020-12-14 11:57:28 +0330

    feat(/composer.json)register uploadfile module

--- uploadfile/src/Http/Controllers/UploadFileController.php
+++ uploadfile/src/Http/Controllers/UploadFileController.php
@@ -5,16 +5,16 @@ namespace Uploadfile\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Carbon;
 
 class uploadfileController extends Controller
 {
     public function create (Request $request)
     {
         $file = $request->file('file');
-        $New_fileName =$file->getClientOriginalName();
-        $pathName = base_path();
-        $file->move($pathName,$New_fileName);
+        $New_fileName =$file->getClientOriginalName() + Carbon::now();
+        $file->move('images',$New_fileName);
+        return $New_fileName;
     }
 
 }
