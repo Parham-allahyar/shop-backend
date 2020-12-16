19fc34da7de9c6193cf04ab5765c6e7e77c956d2
Parham <parhamallahyar70@gmail.com>
2020-12-16 11:06:43 +0330

    feat( acl/src/DataBase/Migrations)Add role,permission,permission_role

---
+++ acl/src/DataBase/Migrations/2014_10_12_000000_create_permissions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionsTable extends Migration
+{
+   
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('lable')->nullable();
+            $table->timestamps();
+        });
+
+
+
+    }
+
+    
+    public function down()
+    {
+        Schema::dropIfExists('permissions');
+       
+    }
+}
