f44bb7fb6877741cf3413fc0c32ec582b69021d6
Parham <parhamallahyar70@gmail.com>
2020-12-16 11:29:06 +0330

    feat(acl/src/Traits/userModelFunctions)add relation whit user Model and permission,role

---
+++ acl/src/Traits/userModelFunctions.php
@@ -0,0 +1,20 @@
+<?php
+namespace ACL\Traits;
+trait userModelFunctions
+{
+
+
+
+    public function hasRole($roles)
+    {
+        return !! $roles->intersect($this->roles)->all();
+    }
+
+    public function hasPermission($permission)
+    {
+        return $this->permissions->contains('name' , $permission->name) || $this->hasRole($permission->roles);
+    }
+
+
+
+}
