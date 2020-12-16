983f6d135cdec1a12cd848e614e470335921bb8e
Parham <parhamallahyar70@gmail.com>
2020-12-16 10:59:23 +0330

    feat(acl/src/Providers/)add AclServiceProvider

---
+++ acl/src/Providers/AclServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ACL\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AclServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . './../Routes/routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/../DataBase/Migrations');
+    }
+
+
+    public function register()
+    {
+    }
+}
