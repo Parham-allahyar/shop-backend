356b048a788a1db033540f6dc6c463265b59f240
Parham <parhamallahyar70@gmail.com>
2020-12-16 11:35:15 +0330

    feat(app/Models/User)use userModelFunctions Trait in User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use ACL\Traits\userModelFunctions;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, userModelFunctions;
 
    
     protected $fillable = [
