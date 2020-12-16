eed4507e1bc8db1db2250f3a7bd731cc4085c782
Parham <parhamallahyar70@gmail.com>
2020-12-16 11:08:35 +0330

    feat( acl/src/DataBase/Models)Add Role and Permission Model

---
+++ acl/src/DataBase/Models/Role.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ACL\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    protected $guarded = [];
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+}
